e5006c41254845fe47a80879308e716d1e558b52
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-17 23:03:12 +0200

    impl basic model enumerator "eq_enum"

---
+++ src/eq_enum.rs
@@ -0,0 +1,132 @@
+type Map<K, V> = indexmap::IndexMap<K, V>;
+
+type ElemIdx = usize;
+type PosIdx = (usize, usize);
+
+#[derive(Clone)]
+struct Constraint(ElemIdx, Term);
+
+#[derive(Clone, PartialEq, Eq)]
+enum Term {
+    Elem(ElemIdx),
+    F(Box<[Term; 2]>),
+}
+
+type Table = Map<PosIdx, ElemIdx>;
+
+#[derive(Clone)]
+struct Ctxt {
+    constraints: Vec<Constraint>,
+    table: Table,
+    n: usize,
+}
+
+pub fn eq_run(n: usize) {
+    step(Ctxt {
+        constraints: build_constraints(n),
+        table: Map::new(),
+        n,
+    });
+}
+
+
+fn step(ctxt: Ctxt) {
+    let all_pos = (0..ctxt.n).map(|x| (0..ctxt.n).map(move |y| (x, y))).flatten();
+    let free_pos = all_pos.filter(|xy| ctxt.table.get(xy).is_none());
+    let Some(pos) = free_pos.max_by_key(|pos| score(*pos, &ctxt)) else {
+        println!("model found:");
+        for x in 0..ctxt.n {
+            for y in 0..ctxt.n {
+                let z = ctxt.table.get(&(x, y)).unwrap();
+                print!("{z} ");
+            }
+            println!("");
+        }
+        return; // We are done!
+    };
+    for e in 0..ctxt.n {
+        // TODO filter out e that conflict with the bijection thing!
+
+        let mut c = ctxt.clone();
+        c.table.insert(pos, e);
+        if simplify(&mut c).is_none() {
+            step(c);
+        }
+    }
+}
+
+struct Failure;
+
+fn simplify(ctxt: &mut Ctxt) -> Option<Failure> {
+    'outer: loop {
+        for Constraint(l, t) in ctxt.constraints.iter_mut() {
+            simplify_term(t, &ctxt.table);
+            match t {
+                Term::F(xy) => if let [Term::Elem(x), Term::Elem(y)] = &**xy {
+                    ctxt.table.insert((*x, *y), *l);
+                    continue 'outer;
+                },
+                Term::Elem(r) => if l != r {
+                    return Some(Failure)
+                },
+            }
+        }
+        return None;
+    }
+}
+
+fn simplify_term(t: &mut Term, tab: &Table) {
+    match t {
+        Term::Elem(e) => {},
+        Term::F(ab) => {
+            let [a, b] = &mut **ab;
+            simplify_term(a, tab);
+            simplify_term(b, tab);
+            if let [Term::Elem(a), Term::Elem(b)] = [a, b] && let Some(new) = tab.get(&(*a, *b)) {
+                *t = Term::Elem(*new);
+            }
+        },
+    }
+}
+
+fn score(pos: PosIdx, ctxt: &Ctxt) -> usize {
+    let mut s = 0;
+    for Constraint(_, t) in &ctxt.constraints {
+        s += count_pos(pos, t);
+    }
+    s
+}
+
+fn count_pos(pos: PosIdx, term: &Term) -> usize {
+    match term {
+        Term::Elem(_) => 0,
+        Term::F(ab) => {
+            if ab[0] == Term::Elem(pos.0) && ab[1] == Term::Elem(pos.1) { 1 }
+            else { count_pos(pos, &ab[0]) + count_pos(pos, &ab[1]) }
+        }
+    }
+}
+
+fn termsize(term: &Term) -> usize {
+    match term {
+        Term::Elem(_) => 1,
+        Term::F(ab) => termsize(&ab[0]) + termsize(&ab[1]) + 1,
+    }
+}
+
+fn build_constraints(n: usize) -> Vec<Constraint> {
+    let mut constraints = Vec::new();
+    let f = |a: &Term, b: &Term| Term::F(Box::new([a.clone(), b.clone()]));
+    for x in 0..n {
+        for y in 0..n {
+            let t = {
+                let x = &Term::Elem(x);
+                let y = &Term::Elem(y);
+
+                f(y, &f(x, &f(&f(y, x), y)))
+            };
+            constraints.push(Constraint(x, t));
+        }
+    }
+    constraints
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ pub use magma::*;
 mod matrix;
 pub use matrix::*;
 
+mod eq_enum;
+pub use eq_enum::*;
+
 fn main() {
-    let m = MatrixMagma::by_fn(5, |x, y| (2*x + 4*y) % 5);
-    dbg!(m.is667());
-    dbg!(m.is225());
-    
+    for i in 1..8 {
+        eq_run(i);
+    }
 }
