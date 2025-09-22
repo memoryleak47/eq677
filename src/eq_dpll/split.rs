838ae8f5a448b48b854720c440218f6eb477528f
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-22 17:52:33 +0200

    ...

--- src/eq_dpll/split.rs
+++ src/eq_dpll/split.rs
@@ -9,23 +9,16 @@ pub fn split_models(ctxt: Ctxt) -> Vec<Ctxt> {
         let mut ctxt = ctxt.clone();
         ctxt.fresh[0] = false;
         ctxt.fresh[1] = false;
-        if propagate((0, 0), 1, &mut ctxt).is_ok() {
-            out.push(ctxt);
-        }
+        assert!(propagate((0, 0), 1, &mut ctxt).is_ok());
+        out.push(ctxt);
     }
 
     {
         let mut ctxt = ctxt.clone();
-        let mut ok = true;
         for i in 0..ctxt.n {
-            if propagate((i, i), i, &mut ctxt).is_err() {
-                ok = false;
-                break;
-            }
+            assert!(propagate((i, i), i, &mut ctxt).is_ok());
         }
-        if ok {
-            out.push(ctxt);
-        }
+        out.push(ctxt);
     }
 
     out
