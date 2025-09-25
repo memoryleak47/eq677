9a2945379847afae67f1e5109a08acd333babe51
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-25 22:41:33 +0200

    add second 677 variant constraint

--- src/sym_dpll/mod.rs
+++ src/sym_dpll/mod.rs
@@ -104,6 +104,17 @@ fn setup_constraints(ctxt: &mut Ctxt) {
             union(x, yxyxy, ctxt);
         }
     }
+
+    for x in 0..ctxt.n {
+        for y in 0..ctxt.n {
+            // x = f(yx, f(f(y, yx), y))
+            let yx = add(y, x, ctxt);
+            let yyx = add(y, yx, ctxt);
+            let yyxy = add(yyx, y, ctxt);
+            let yxyyxy = add(yx, yyxy, ctxt);
+            union(x, yxyyxy, ctxt);
+        }
+    }
     rebuild(ctxt);
 }
 
