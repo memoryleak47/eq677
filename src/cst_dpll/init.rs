9a78155765e4077d0694683231c84df1a233679c
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-07 01:28:33 +0200

    impl cst_dpll/init

--- src/cst_dpll/init.rs
+++ src/cst_dpll/init.rs
@@ -1,5 +1,25 @@
 use crate::cst_dpll::*;
 
 pub fn build_ctxt(n: usize) -> Ctxt {
-    todo!()
+    let mut ctxt = Ctxt {
+        trail: Vec::new(),
+        n,
+        classes: std::iter::repeat(Class::Pending(SmallVec::new()))
+            .take(n*n)
+            .collect(),
+    };
+    add_constraints(n, &mut ctxt);
+    ctxt
+}
+
+fn add_constraints(n: usize, ctxt: &mut Ctxt) {
+    for x in 0..(n as u8) {
+        for y in 0..(n as u8) {
+            let p = mk_p(x, y, n);
+            let mut v = SmallVec::new();
+            v.push(C::C1M0(y, x)); // TODO right way around?
+            v.push(C::C2M0(y, x));
+            ctxt.classes[p as usize] = Class::Pending(v);
+        }
+    }
 }
--- src/cst_dpll/mod.rs
+++ src/cst_dpll/mod.rs
@@ -4,6 +4,9 @@ use smallvec::SmallVec;
 mod init;
 use init::*;
 
+mod p;
+use p::*;
+
 mod run;
 pub use run::*;
 
@@ -14,6 +17,7 @@ type E = u8;
 type P = u16;
 
 // Constraint
+#[derive(Clone)]
 enum C {
     C1M0(/*x*/ E, /*y*/ E),                     // x = y*(x*((y*x)*y))
     C1M1(/*x*/ E, /*y*/ E, /*a*/ E),            // x = y*(x*(  a  *y))
@@ -24,6 +28,7 @@ enum C {
     C2M2(/*x*/ E, /*y*/ E, /*yx*/ E, /*a*/ E),  // x = yx    * (a  * y)
 }
 
+#[derive(Clone)]
 enum Class {
     Evaluated(E),
     Pending(SmallVec<[C; 4]>), // the constraints that currently wait on us.
@@ -31,7 +36,8 @@ enum Class {
 
 struct Ctxt {
     trail: Vec<TrailEvent>,
-    classes: Vec<Class>,
+    classes: Box<[Class]>,
+    n: usize,
 }
 
 enum TrailEvent {
