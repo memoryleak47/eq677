1edbf1affb43596dd9868ef94fe9890f2f3ca7dd
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-25 19:08:51 +0200

    ...

--- src/kb/mod.rs
+++ src/kb/mod.rs
@@ -11,7 +11,34 @@ pub enum Term {
 
 // returns whether we can prove s > t:
 fn kbo(s: &Term, t: &Term) -> bool {
+    if !var_gte(s, t) { return false }
     todo!()
+}
+
+// checks whether every variable from t occurs at least equally often in s.
+fn var_gte(s: &Term, t: &Term) -> bool {
+    (0..var_c(t)).all(|v|
+        var_count(s, v) >= var_count(t, v)
+    )
+}
+
+// returns 0 if t contains no variables.
+// returns v+1 if v is the highest variable in t.
+fn var_c(t: &Term) -> V {
+    match t {
+        Term::Var(v) => v+1,
+        Term::Elem(_) => 0,
+        Term::F(b) => var_c(&b[0]).max(var_c(&b[1])),
+    }
+}
+
+// Counts how often the variable v occurs in t.
+fn var_count(t: &Term, v: V) -> u32 {
+    match t {
+        Term::Var(v2) => (v == *v2) as u32,
+        Term::Elem(_) => 0,
+        Term::F(b) => var_count(&b[0], v) + var_count(&b[1], v),
+    }
 }
 
 fn weight(t: &Term) -> u32 {
