3940103dcc51bf0f84fc91c69b4fc0a3c4ad8d0d
memoryleak47 <memoryleak47@protonmail.ch>
2025-11-01 09:38:30 +0100

    cycle-class: re-add "x*x = x" case

--- src/c_dpll/split.rs
+++ src/c_dpll/split.rs
@@ -7,17 +7,14 @@ pub fn split_models_via_row(ctxt: Ctxt) -> Vec<Ctxt> {
 
     // We choose 0 to maximize |C(0,0)|, thus if 0*0 = 0, then x*x = x holds generally.
     // Further, in this case we can still assume freshness.
-    // TODO re-add:
-/*
     {
         let mut ctxt = ctxt.clone();
         for i in 0..ctxt.n {
             assert!(prove_triple(i, i, i, &mut ctxt).is_ok());
         }
         assert!(propagate(&mut ctxt).is_ok());
-        out.push(ctxt);
+        split_rest(1, 1, ctxt, &mut out);
     }
-*/
 
     // zero_orbit_size = 1 is covered by the case above;
     // zero_orbit_size in [2, 3] can't be, as no self-producing cycles are of length 2 or 3.
@@ -52,6 +49,7 @@ fn split_rest(last_size: E, next_idx: E, ctxt: Ctxt, out: &mut Vec<Ctxt>) {
             let ii = next_idx + i;
             let ii_1 = next_idx + (i+1)%next_cycle;
             ctxt.cycle_class[ii as usize] = next_cycle;
+            // TODO learn when this can fail
             if prove_triple(0, ii, ii_1, &mut ctxt).is_err() { continue 'outer; }
         }
         split_rest(next_cycle, next_idx + next_cycle, ctxt, out)
