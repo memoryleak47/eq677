1f08928d542d0c716c205648e26eedc7908ee64f
memoryleak47 <memoryleak47@protonmail.ch>
2025-11-15 02:16:46 +0100

    have sensible db order

--- build.rs
+++ build.rs
@@ -7,6 +7,8 @@ fn main() {
 
     let mut out = String::from("pub static DB_SOURCES: &[(&str, &str)] = &[");
 
+    let mut files = Vec::new();
+
     for entry in fs::read_dir("db").unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
@@ -14,7 +16,12 @@ fn main() {
         let fname = path.file_name().unwrap().to_str().unwrap();
         let path_str = path.to_str().unwrap();
 
+        files.push(fname.to_string());
+    }
 
+    files.sort_by_cached_key(decompose_filename);
+
+    for fname in files {
         out.push_str(&format!(
             "(\"{fname}\", include_str!(concat!(env!(\"CARGO_MANIFEST_DIR\"), \"/db/{fname}\"))),\n",
         ));
@@ -25,4 +32,15 @@ fn main() {
     fs::write(dest, out).unwrap();
 
     println!("cargo:rerun-if-changed=db");
+}
+
+fn decompose_filename(x: &String) -> (u32, u32) {
+    let mut it = x.split("_");
+    let a = it.next().unwrap();
+    let b = it.next().unwrap();
+    assert!(it.next().is_none());
+
+    let a = a.parse::<u32>().unwrap();
+    let b = b.parse::<u32>().unwrap();
+    (a, b)
 }
