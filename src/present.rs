e3f8a09367af6d60e7bb8fa3a96a463f6224138a
memoryleak47 <memoryleak47@protonmail.ch>
2025-12-01 22:01:24 +0100

    add PRESENT_DB

--- src/present.rs
+++ src/present.rs
@@ -4,29 +4,49 @@ use std::sync::Mutex;
 
 lazy_static::lazy_static! {
     pub static ref PRINT_MUTEX: Mutex<()> = Mutex::new(());
+    pub static ref PRESENT_DB: Mutex<HashSet<MagmaName>> = Mutex::new(HashSet::new());
 }
 
 const CHECK_COMPOSITE: bool = false;
 const SHOW_AUTOM: bool = false;
 
+pub fn get_present_db() -> Vec<MagmaName> {
+    let handle = PRESENT_DB.lock().unwrap();
+    let mut v: Vec<_> = (*handle).iter().cloned().collect();
+    drop(handle);
+    v.sort();
+    v
+}
+
+pub fn present_db_contains(name: MagmaName) -> bool {
+    let handle = PRESENT_DB.lock().unwrap();
+    handle.contains(&name)
+}
+
+pub fn present_db_add(name: MagmaName) -> bool {
+    let mut handle = PRESENT_DB.lock().unwrap();
+    handle.insert(name)
+}
+
 pub fn present_model(n: usize, finder: &str, f: impl Fn(usize, usize) -> usize) {
     let magma = MatrixMagma::by_fn(n, f);
 
-    let mut new_str = "";
+    let mut globally_new_str = "";
     if n <= 100 {
-        let (name_, new_) = db_intern(&magma);
-        if new_ {
-            new_str = "New ";
+        let (name, globally_new) = db_intern(&magma);
+        if !present_db_add(name) { return; }
+        if globally_new {
+            globally_new_str = "New ";
         }
     }
 
     let mut print_handle = PRINT_MUTEX.lock().unwrap();
 
     if n <= 100 {
-        println!("{new_str}Model of size {n} found by {finder}:");
+        println!("{globally_new_str}Model of size {n} found by {finder}:");
         magma.canonicalize2().dump();
     } else {
-        println!("{new_str}Model of size {n} found by {finder}");
+        println!("{globally_new_str}Model of size {n} found by {finder}");
     }
 
     if CHECK_COMPOSITE {
