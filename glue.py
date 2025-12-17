b010e1205ff690f6d77a0e5a7a968c441efa53ae
memoryleak47 <memoryleak47@protonmail.ch>
2025-12-17 02:15:22 +0100

    add projective plane of order 4 to glue.py

--- glue.py
+++ glue.py
@@ -6,7 +6,7 @@ import random
 # If you want another model, change this seed.
 random.seed(1)
 
-# Choose 5 or 11
+# Choose block-size of 5 or 11
 n = 5
 
 def choose_block_magma():
@@ -38,6 +38,48 @@ def dump(m):
             z = m[(x, y)]
             print(points.index(z), end=" ")
         print()
+
+
+def generate_projective_plane_order_4():
+    def gf4_add(a, b):
+        return a ^ b
+
+    def gf4_mul(a, b):
+        # Multiplication table for GF(4) with primitive polynomial x^2 + x + 1
+        table = [
+            [0, 0, 0, 0],
+            [0, 1, 2, 3],
+            [0, 2, 3, 1],
+            [0, 3, 1, 2]
+        ]
+        return table[a][b]
+
+    n = 4
+    blocks = []
+
+    # 1. Non-vertical lines: y = m*x + b using GF(4) math
+    for m in range(n):
+        for b in range(n):
+            line = []
+            for x in range(n):
+                # y = gf4_add(gf4_mul(m, x), b)
+                y = gf4_mul(m, x) ^ b
+                line.append(x * n + y)
+            line.append(16 + m)
+            blocks.append(line)
+
+    # 2. Vertical lines: x = c
+    for c in range(n):
+        line = []
+        for y in range(n):
+            line.append(c * n + y)
+        line.append(20)
+        blocks.append(line)
+
+    # 3. Line at infinity
+    blocks.append(list(range(16, 21)))
+
+    return list(range(21)), blocks
 
 def generate_affine_plane():
     points = [(x, y) for x in range(n) for y in range(n)]
@@ -72,7 +114,7 @@ def random_glue(points, blocks):
                 m[(block[x], block[y])] = block[m5[(x, y)]]
     return m
 
-points, blocks = generate_affine_plane()
+points, blocks = generate_projective_plane_order_4()
 m = random_glue(points, blocks)
 
 check677(m)
