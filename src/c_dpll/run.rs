8d533d78296f87fab3430c593e93da015b0eea7b
memoryleak47 <memoryleak47@protonmail.ch>
2026-07-21 01:39:58 +0200

    ...

--- src/c_dpll/run.rs
+++ src/c_dpll/run.rs
@@ -83,7 +83,7 @@ fn submit_model(ctxt: &Ctxt) {
 
 pub fn defresh(e: E, ctxt: &mut Ctxt) {
     if e >= ctxt.nonfresh {
-        // assert!(e == ctxt.nonfresh);
+        assert!(e == ctxt.nonfresh);
         ctxt.nonfresh += 1;
         ctxt.trail.push(TrailEvent::Defresh);
     }
